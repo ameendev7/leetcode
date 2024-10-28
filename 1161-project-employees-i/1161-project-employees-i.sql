6a13ddf776393e3accfd276d9abce531716cd29b
ameendev7 <130288767+ameendev7@users.noreply.github.com>
2024-10-28 16:11:21 +0530

    Time: 1167 ms (92.62%), Space: 0B (100%) - LeetHub

---
+++ 1161-project-employees-i/1161-project-employees-i.sql
@@ -0,0 +1,2 @@
+/* Write your T-SQL query statement below */
+select p.project_id  ,round(avg(experience_years*1.0 ),2)as average_years from Project p join Employee e on p.employee_id =e.employee_id group by project_id  ;
