1f25aa01cf2e43097276c740165ea4dd0c4e1c5a
ameendev7 <130288767+ameendev7@users.noreply.github.com>
2024-12-24 15:44:24 +0530

    Time: 58 ms (54.07%), Space: 39.8 MB (46.67%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.cs
@@ -1,20 +1,19 @@
 public class Solution {
     public string MergeAlternately(string word1, string word2) {
-        StringBuilder str= new StringBuilder();
-        int i=0,j=0;
-        while(i<word1.Length || j<word2.Length)
-        {
-            if(i<word1.Length)
-            {
-                str.Append(word1[i]);
-                i++;
-            }
-            if(j<word2.Length)
-            {
-                str.Append(word2[j]);
-                j++;
-            }
+     StringBuilder res=new StringBuilder();
+     int i=0,j=0;
+     while(i<word1.Length&&j<word2.Length){
+        res.Append(word1[i]).Append(word2[j]);
+        i++;
+        j++;}
+        while(i<word1.Length){
+            res.Append(word1[i]);
+            i++;
         }
-        return str.ToString();
+        while(j<word2.Length){
+            res.Append(word2[j]);
+            j++;
+        }
+             return res.ToString();
     }
 }
